82a90e711c35d569a94c2e33bf527ad4cc540566
fkantelberg <florian.kantelberg@initos.com>
2019-05-28 12:11:35 +0200

    Use parse_commands in webhook

--- src/oca_github_bot/webhooks/on_pr_review_comment.py
+++ src/oca_github_bot/webhooks/on_pr_review_comment.py
@@ -1,8 +1,8 @@
 # Copyright (c) initOS GmbH 2019
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
 
+from ..commands import parse_commands
 from ..router import router
-from ..tasks.merge_bot import merge_bot
 
 
 @router.register("pull_request_review_comment")
@@ -12,7 +12,7 @@ async def on_pr_review_comment(event, gh, *args, **kwargs):
     pr = event.data["pull_request"]["number"]
     target_branch = event.data["pull_request"]["base"]["ref"]
     user = event.data["comment"]["user"]
+    text = event.data["comment"]["body"]
 
-    with github.repository(org, repo) as gh_repo:
-        if git_user_can_push(gh_repo, username):
-            merge_bot_start(org, repo, pr, target_branch)
+    for command in parse_commands(text):
+        command.run(org, repo, pr, target_branch, user)
